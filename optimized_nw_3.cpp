e6f54a51f4bc6696b3d4049e14aaac57ac351e92
Pranil <praniltripathi7290@gmail.com>
2024-04-06 12:18:02 +0530

    added optimized_3 file

--- optimized_nw_3.cpp
+++ optimized_nw_3.cpp
@@ -1,10 +1,14 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+
+
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
 #include <bits/stdc++.h>
 #include <sys/time.h>
-
+#include <omp.h>
 
 #include "helper.hpp"
 
@@ -20,32 +24,48 @@ double needleman_wunsch(string &A, string &B, int match_penalty, int mismatch_pe
 
     struct timeval t;
     start(&t);
-    
+
+    #pragma omp parallel for    
     for (int i=0; i<=sequence1_size; i++) {
         dp[i][0] = i * gap_penalty;
     }
     
+    #pragma omp parallel for
     for (int i=0; i<=sequence2_size; i++) {
         dp[0][i] = i * gap_penalty;
     }
 
-    for (int j=1; j<=sequence2_size; j++) {
-        for (int i=1; i<=sequence1_size; i++) {
+    for (int z=1; z<=sequence2_size; z++) {
+        int i=1,j=z;
+        #pragma omp parallel for
+        for(;i<=sequence1_size && j>=1;){
             int score = A[i-1] == B[j-1] ? match_penalty : mismatch_penalty;
             dp[i][j] = max(dp[i-1][j-1] + score, max(dp[i-1][j] + gap_penalty, dp[i][j-1] + gap_penalty));
-            }
+            i++;j--;
+        }
+    }
+
+    for (int z=2; z<=sequence1_size; z++) {
+        int j=sequence2_size,i=z;
+        #pragma omp parallel for
+        for(;i<=sequence1_size && j>=1;){
+            int score = A[i-1] == B[j-1] ? match_penalty : mismatch_penalty;
+            dp[i][j] = max(dp[i-1][j-1] + score, max(dp[i-1][j] + gap_penalty, dp[i][j-1] + gap_penalty));
+            i++;j--;
+        }
     }
 
     double time = end(&t);
 
     return time;
-    // return gflops / (time * 1e9);
+    //return gflops / (time * 1e9);
 
 }
 
+
 int main() {
 
-    ofstream fout("brute_output.txt");
+    ofstream fout("optimized_3_output.txt");
 
     for (int i=1; i<=1000; i++) {
 
@@ -59,5 +79,3 @@ int main() {
     }
     fout.close();
 }
-
-
